ba104ba47698df410fe5664ca0f3ac331ad848ee
Peter <peter@MacBook-Air-de-Peter.local>
2021-08-04 11:07:14 +0200

    gamedetails ok

--- src/components/game/GameDetails.jsx
+++ src/components/game/GameDetails.jsx
@@ -4,10 +4,24 @@ import axios from 'axios'
 import {Link} from 'react-router-dom'
 
 
+function GameDetails(props) {
+    const [trailer, settrailer] = useState();
+    const gameId = props.match.params.id
 
-function GameDetails() {
+
+    useEffect(() => {
+        axios.get(`https://apis.wilders.dev/wild-games/games/${gameId}`)
+        .then((res) =>{
+            settrailer(res.data.clip.clip)
+        })
+        
+    }, [gameId])
+
     return (
-        <div>
+        <div className="GameDatails">
+            <span> Clip Videos :</span>
+            <video control src={traiter}/>
+            <link to={{pathname: '/'}}>Retour vers l'acceuil</link>
             
         </div>
     )
